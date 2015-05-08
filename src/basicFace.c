b0f29156e8ad2a3cca6d323022e7298fc1ffcc1b
Steve Groves <stevegroves123@gmail.com>
2015-05-08 16:29:59 +0100

    Added text layers
    Added two text layers to the screen, top one says Pebble and the bottom one says time.

--- src/basicFace.c
+++ src/basicFace.c
@@ -1,7 +1,8 @@
 #include <pebble.h>
 static Window *s_main_window;
 static TextLayer *s_time_layer;
-static TextLayer *s_message_layer;
+static TextLayer *s_messagetime_layer;
+static TextLayer *s_messagepebble_layer;
 
 static void update_time() {
   // Get a tm structure
@@ -31,27 +32,36 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 
 static void main_window_load(Window *window)
   {
-  s_time_layer = text_layer_create(GRect(0, 50, 144, 50));
+  s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
-  s_message_layer = text_layer_create(GRect (0, 120, 144, 50));
-  text_layer_set_background_color(s_message_layer, GColorBlack);
-  text_layer_set_text_color(s_message_layer, GColorWhite);  
-  text_layer_set_text(s_message_layer, "time");
-  text_layer_set_font(s_message_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  s_messagetime_layer = text_layer_create(GRect (0, 120, 144, 50));
+  text_layer_set_background_color(s_messagetime_layer, GColorBlack);
+  text_layer_set_text_color(s_messagetime_layer, GColorWhite);  
+  text_layer_set_text(s_messagetime_layer, "time ");
+  text_layer_set_font(s_messagetime_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
+
+  s_messagepebble_layer = text_layer_create(GRect (0, 0, 144, 50));
+  text_layer_set_background_color(s_messagepebble_layer, GColorBlack);
+  text_layer_set_text_color(s_messagepebble_layer, GColorWhite);  
+  text_layer_set_text(s_messagepebble_layer, "Pebble ");
+  text_layer_set_font(s_messagepebble_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28)); 
+  text_layer_set_text_alignment(s_messagepebble_layer, GTextAlignmentRight);
   
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_message_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_messagetime_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_messagepebble_layer));
 }
 
 static void main_window_unload(Window *window)
   {
   text_layer_destroy(s_time_layer);
-  text_layer_destroy(s_message_layer);
+  text_layer_destroy(s_messagetime_layer);
+  text_layer_destroy(s_messagepebble_layer);
 }
 
 static void init()
